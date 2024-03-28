bb6933ea415baedd8661bb2cf6fe5f7af8855ef6
Jordan Waite <43104679+jordanwaite@users.noreply.github.com>
2024-03-28 12:03:15 -0400

    add anagram logic

--- ArraysAndHashing/ContainsDuplicate.cs
+++ ArraysAndHashing/ContainsDuplicate.cs
@@ -9,8 +9,27 @@ public class ArraysAndHashing
 
     public static bool IsAnagram(string s, string t)
     {
+        if (s.Length != t.Length)
+            return false;
 
+        Dictionary<char, int> map = new();
 
-        return false;
+        foreach (char c in s)
+        {
+            if (!map.ContainsKey(c))
+                map.Add(c, 0);
+            map[c]++;
+        }
+
+        foreach (char c in t)
+        {
+            if (!map.ContainsKey(c) || map[c] == 0)
+            {
+                return false;
+            }
+            map[c]--;
+        }
+
+        return true;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine($"The int array contains duplicate numbers: {ArraysAndHashing.ContainsDuplicate([1, 2, 3, 4, 5])}");
 
-Console.WriteLine($"The two strings are anagrams: {ArraysAndHashing.IsAnagram("car", "rac")}");
+Console.WriteLine($"The two strings are anagrams: {ArraysAndHashing.IsAnagram("carc", "racc")}");
